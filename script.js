7f3bcd5d19385375ef7eadcc56863b9ff97e7bcf
DarioM1992 <dario21992@gmail.com>
2026-05-08 17:55:07 +0200

    esercizio finito

---
+++ script.js
@@ -0,0 +1,19 @@
+const age = parseInt(prompt('dammi una età a caso'));
+const kms = parseInt(prompt('dammi un kilometraggio a caso'));
+const ticketPriceEveryKm = 0.21 ;
+const total = (kms * ticketPriceEveryKm);
+
+let finalPrice = total;
+
+if (age < 18){
+    const discount = (total * 20) / 100;
+    finalPrice = total - discount;
+} else if (age >= 65 ) {
+    const discount = (total *40) / 100;
+    finalPrice = total - discount;
+} else {
+    finalPrice = total;
+}
+    
+console.log (`il prezzo finale del biglietto è : ${finalPrice.toFixed(2)}€`)
+
